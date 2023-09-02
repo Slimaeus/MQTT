f89b53b50be9fdaff0c5f0a1766faf8c4f322c4d
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-02 23:23:46 +0700

    split to publisher and consumer

---
+++ MQTT.Api/HelloConsumer.cs
@@ -0,0 +1,34 @@
+﻿using MQTTnet.Client;
+using System.Text;
+
+namespace MQTT.Api;
+
+public class HelloConsumer : BackgroundService
+{
+    private readonly IMqttClient _mqttClient;
+
+    public HelloConsumer(IMqttClient mqttClient)
+    {
+        _mqttClient = mqttClient;
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+            if (_mqttClient.IsConnected)
+            {
+                await _mqttClient.SubscribeAsync("/test/hello", cancellationToken: stoppingToken);
+                {
+                    _mqttClient.ApplicationMessageReceivedAsync += e =>
+                    {
+                        if (e.ApplicationMessage != null)
+                        {
+                            Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment)}");
+                        }
+                        return Task.CompletedTask;
+                    };
+                    await Task.Delay(1000, stoppingToken);
+                }
+                break;
+            }
+    }
+}
---
+++ MQTT.Api/HelloPublisher.cs
@@ -0,0 +1,39 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+using System.Text;
+using System.Text.Json;
+
+namespace MQTT.Api;
+
+public class HelloPublisher : BackgroundService
+{
+    private readonly IMqttClient _mqttClient;
+
+    public HelloPublisher(IMqttClient mqttClient)
+    {
+        _mqttClient = mqttClient;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+
+            if (_mqttClient.IsConnected)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var message = new MqttApplicationMessageBuilder()
+                        .WithTopic("/test/hello")
+                        .WithPayload(Encoding.ASCII.GetBytes(JsonSerializer.Serialize(new { content = $"Hello, MQTT! Message number {i}" })))
+                        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                        .WithRetainFlag()
+                        .Build();
+
+                    await _mqttClient.PublishAsync(message, stoppingToken);
+                    await Task.Delay(1000, stoppingToken); // Wait for 1 second
+                }
+                break;
+            }
+    }
+}
